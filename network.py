f4e6dc6d3c11f0a60a233c024e99a173a7889bf8
RomanShafranyuk <73229151+RomanShafranyuk@users.noreply.github.com>
2024-03-10 12:20:12 +0400

    Исправление багов передачи блоков

--- network.py
+++ network.py
@@ -15,27 +15,27 @@ def send_message(sock: socket.socket, transactions: list):
                     transactions (list) : список транзакций для отправки
     """
     global PACKAGE_SIZE
-    len_packages = []
-    message = json.dumps(transactions)
+    message = json.dumps(transactions) # msg string
     message_size = sys.getsizeof(message)
-    sock.send(str(message_size).encode("utf-8"))
-    print(sock.recv(PACKAGE_SIZE).decode("utf-8"))
-    bar = tqdm(range(message_size), f"Отправка транзакций {message_size} байт", unit="B", unit_scale=True, unit_divisor=PACKAGE_SIZE)
-    start_msg = 0
-    end_msg = PACKAGE_SIZE
+    sock.send(message_size.to_bytes(4, 'little')) # send message size
+    print(sock.recv(PACKAGE_SIZE).decode("utf-8")) # recv system message
+    bar = tqdm(range(message_size), f"Отправка транзакций {message_size} байт")#, unit="B", unit_scale=True, unit_divisor=PACKAGE_SIZE)
 
+    ind = 0
     while True:
-        data = message[start_msg:end_msg]
-        len_packages.append(len(data))
-        if not data or len(data) == 0:
-            break
+        if ind + PACKAGE_SIZE >= message_size:
+            pack_size = message_size - ind
+        else:
+            pack_size = PACKAGE_SIZE
+        data = message[ind:ind+pack_size]
         sock.send(data.encode("utf-8"))
         sock.recv(PACKAGE_SIZE).decode("utf-8")
         bar.update(len(data))
-        start_msg += PACKAGE_SIZE
-        end_msg += PACKAGE_SIZE
-    print(len_packages)
-    print("Транзакции успешно отправлены")
+        ind += pack_size
+        if ind >= message_size:
+            break
+    
+    print(f"{message_size}, {ind}\n Транзакции успешно отправлены")
 
 def receive_message(sock: socket.socket):
     """
@@ -51,25 +51,19 @@ def receive_message(sock: socket.socket):
     """
     global PACKAGE_SIZE
     len_packages = []
-    message = None
-    msg_size = int(sock.recv(4).decode("utf-8"))
+    message = bytearray()
+    msg_size = int.from_bytes(sock.recv(4), 'little')
     sock.send(f"Получен размер файла, {msg_size} байт".encode("utf-8"))
     bar = tqdm(range(msg_size), f"Отправка транзакций {msg_size} байт", unit="B", unit_scale=True, unit_divisor=PACKAGE_SIZE)
     while True:
         data = sock.recv(PACKAGE_SIZE)
-        len_packages.append(len(data))
         if not data:
             break
-        if message == None:
-            message = data
-        else:
-            message += data
+        len_packages.append(len(data))
+        message.extend(data)
         sock.send("Получено сообщение".encode("utf-8"))
         bar.update(len(data))
     
     print(f"\n{sys.getsizeof(message)} байт\n")
     print(len_packages)
     return json.loads(message.decode("utf-8"))
-
-
-    
